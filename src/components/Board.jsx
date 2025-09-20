7cd67c22171b51e487b783007984b7c2265656e0
Roel Crodua <roelcrodua@yahoo.com>
2025-09-20 00:56:49 -0400

    Added more card entries and details

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,12 +3,21 @@ import Card from './Card'
 const Board = () => {
     return (
         <div className='board'>
-            <Card event='Lunch & Learn for Seniors 55+' location='Village Center Complex' date='September 12, 2025' time='11:00 AM - 12:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6949' details='https://vpsfl.org/Calendar.aspx?EID=3295'/>
-            <Card event='Hispanic Heritage Festival' location='Village Center Complex' date='September 19, 2025' time='6:00 PM - 8:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6991' details='https://vpsfl.org/Calendar.aspx?EID=3364'/>
-            <Card event='Lotería (Mexican Bingo) at the Library' location='Palm Springs Public Library' date='September 29, 2025' time='3:00 PM - 4:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6959' details='https://vpsfl.org/Calendar.aspx?EID=3328'/>
-            <Card event='55+ Senior Health Fair' location='Village Center Complex' date='October 18, 2025' time='9:00 AM - 12:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6950' details='https://vpsfl.org/Calendar.aspx?EID=3296'/>
-            <Card event='Senior Lunch & Learn' location='Village Center Complex' date='August 13, 2025' time='
-11:30 AM - 12:30 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6911' details='https://vpsfl.org/Calendar.aspx?EID=3291'/>
+            <Card event='Lunch & Learn for Seniors 55+' location='Village Center Complex' date='September 12, 2025' time='11:00 AM - 12:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6949' details='https://vpsfl.org/Calendar.aspx?EID=3295' />
+            <Card event='Hispanic Heritage Festival' location='Village Center Complex' date='September 19, 2025' time='6:00 PM - 8:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6991' details='https://vpsfl.org/Calendar.aspx?EID=3364' />
+            <Card event='Lotería (Mexican Bingo) at the Library' location='Palm Springs Public Library' date='September 29, 2025' time='3:00 PM - 4:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6959' details='https://vpsfl.org/Calendar.aspx?EID=3328' />
+            <Card event='55+ Senior Health Fair' location='Village Center Complex' date='October 18, 2025' time='9:00 AM - 12:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6950' details='https://vpsfl.org/Calendar.aspx?EID=3296' />
+            <Card event='Senior Lunch & Learn' location='Village Center Complex' date='August 13, 2025' time='11:30 AM - 12:30 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6911' details='https://vpsfl.org/Calendar.aspx?EID=3291' />
+            <Card event='Lego Club at Palm Springs Public Library' location='Public Library' date='October 1, 2025' time='3:00 PM - 4:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6967' details='https://vpsfl.org/Calendar.aspx?EID=3349' />
+            <Card event='English for Families' location='Palm Springs Public Library' date='September 23, 2025' time='4:00 PM - 5:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6953' details='https://vpsfl.org/Calendar.aspx?EID=3320' />
+            <Card event='Museum-on-the-go for k-2 graders' location='Palm Springs Public Library' date='September 25, 2025' time='3:30 PM - 4:30 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6963' details='https://vpsfl.org/Calendar.aspx?EID=3341' />
+            <Card event='College Bound Blueprint' location='Palm Springs Public Library' date='September 25, 2025' time='5:30 PM - 6:30 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6960' details='https://vpsfl.org/Calendar.aspx?EID=3329' />
+            <Card event='Coffee with a Cop' location='Dunkin Donuts – 3060 S Congress Ave, Palm Springs' date='August 7, 2025' time='8:30 AM - 10:30 AM' image='https://vpsfl.org/ImageRepository/Document?documentID=6948' details='https://vpsfl.org/Calendar.aspx?EID=3294' />
+            <Card event='A Night in the Forest' location='Palm Springs Public Library' date='August 7, 2025' time='5:30 PM - 6:30 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6956' details='https://vpsfl.org/Calendar.aspx?EID=3321' />
+            <Card event='Domino Cafe' location='Palm Springs Public Library' date='August 12, 2025' time='11:00 AM - 12:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6961' details='https://vpsfl.org/Calendar.aspx?EID=3331' />
+            <Card event='Teen Book Club' location='Palm Springs Public Library' date='August 14, 2025' time='4:00 PM - 5:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6965' details='https://vpsfl.org/Calendar.aspx?EID=3343' />
+            <Card event='Busch Wildlife Adventure- For Seniors 55+' location='Village Center Complex' date='August 16, 2025' time='10:00 AM - 1:00 PM' image='https://vpsfl.org/ImageRepository/Document?documentID=6912' details='https://vpsfl.org/Calendar.aspx?EID=3292' />
+
         </div>
 
     )
