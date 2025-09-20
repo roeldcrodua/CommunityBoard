9edb5c126933f9b62baa3fce47656623f04df3fc
Roel Crodua <roelcrodua@yahoo.com>
2025-09-20 00:55:25 -0400

    Added logo with flex container

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ const App = () => {
 
   return (
     <div className="App">
-
-      <h1>Welcome to the Village of Palm Springs</h1>
+      <div className='flex-container'>
+        <img className='logo' src='https://vpsfl.org/ImageRepository/Document?documentID=6054' />
+        <h1 className='title-header'>Welcome to <br /> Village of Palm Springs</h1>
+      </div>
+      <h1 className='event-header'>List of Recent and Future Community Events</h1>
       <Board />
 
 
