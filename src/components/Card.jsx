994ba419d862a95ad6074a884fda868d1063ba5c
Roel Crodua <roelcrodua@yahoo.com>
2025-09-24 00:19:34 -0400

    Added classNames

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ const Card = (props) => {
       <div className='card-body'>
         <img src={props.image} />
         <h3 className='card-title'>{props.event}</h3>
-        <h4>{props.location}</h4>
-        <h5>{props.date}</h5>
-        <h5>{props.time}</h5>
+        <h4 className='location'>{props.location}</h4>
+        <h5 className='date'>{props.date}</h5>
+        <h5 className='time'>{props.time}</h5>
         <a href={props.details} className='details'>More details . . .</a>
       </div>
 
